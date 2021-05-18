630cf94b355217a9d0f62b5295ca7eddbd903005
ariju <jun.0309ari@gmail.com>
2021-05-18 15:48:59 +0900

    setup react context provider

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,10 @@ import React, { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import EventForm from "./EventForm";
 import Events from "./Events";
+import AppContext from "../contexts/AppContext";
 import reducer from "../reducers";
+
+console.log({ AppContext });
 
 //useReducer第一引数にreducer第二引数defaultでどうしたいか
 //第三引数初期化時の行いたい処理
@@ -10,10 +13,12 @@ const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={'Hello, I am aProvider.'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
